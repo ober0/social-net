42cdb6f2bc0618afdd0da4eaf9d7b9744fbd7956
Ruslan <onishruslan@yandex.ru>
2024-07-07 00:11:47 +0300

    added function - adding photos

--- static/js/user-section-1.js
+++ static/js/user-section-1.js
@@ -44,10 +44,132 @@ document.getElementById('open-all-video').addEventListener('click', function () 
     window.location.href = '/video?user=' + this.getAttribute('tag')
 })
 
-function newPhoto() {
-    window.location.href = '/photos/add'
-}
 
 function newVideo() {
     window.location.href = '/video/add'
 }
+
+
+function cancelLoad() {
+    document.getElementById('body').style.opacity = 1;
+    document.getElementById('new-photo-menu').classList.add('hide');
+    let filesToRemove = document.querySelectorAll('.new-photo-img')
+    filesToRemove.forEach(file => {
+        file.remove()
+    })
+    document.getElementById('cancel-load-photo').removeEventListener('click', cancelLoad)
+}
+
+
+function newPhoto() {
+    console.log(1)
+    document.getElementById('newPhoto-input').click()
+}
+
+document.getElementById('newPhoto-input').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+
+    let fileTypesAccess = ['image/png', 'image/jpeg'];
+    if (file) {
+        if (fileTypesAccess.includes(file.type)) {
+            socketio.emit('newPhoto', {
+                file: file,
+                filename: file.name
+            });
+        } else {
+            alert('Недопустимый тип файла. Пожалуйста, выберите файл .png или .jpg')
+        }
+    }
+});
+
+
+socketio.on('newPhoto_result', (data) => {
+    if (data.success){
+        document.getElementById('body').style.opacity = 0.2;
+        document.getElementById('new-photo-menu').classList.remove('hide');
+        let file = document.getElementById('newPhoto-input').files[0]
+
+        if (file){
+            let reader = new FileReader();
+
+            reader.onload = function (e){
+                let parent = document.getElementById('photo-container')
+                let newImg = document.createElement('img')
+                newImg.src = e.target.result
+                newImg.classList.add('new-photo-img')
+                newImg.width = 200
+                newImg.height = 200
+                newImg.setAttribute('filename', file.name)
+                parent.appendChild(newImg)
+                document.getElementById('newPhoto-input').value = ''
+                console.log(2)
+            }
+            reader.readAsDataURL(file);
+        }
+
+    }
+    else {
+        alert(data.error)
+    }
+})
+
+document.getElementById('add-new-photo').addEventListener('click', function () {
+    document.getElementById('add_new_photo-input').click()
+})
+
+
+document.getElementById('add_new_photo-input').addEventListener('change', function () {
+    let reader = new FileReader()
+
+    let fileTypesAccess = ['image/png', 'image/jpeg'];
+    let file = this.files[0]
+    if (file) {
+        if (fileTypesAccess.includes(this.files[0].type)) {
+             reader.onload = function (e){
+                let parent = document.getElementById('photo-container')
+                let newImg = document.createElement('img')
+                newImg.src = e.target.result
+                newImg.classList.add('new-photo-img')
+                newImg.width = 200
+                newImg.height = 200
+                newImg.setAttribute('filename', file.name)
+                parent.appendChild(newImg)
+
+                let allFiles = document.querySelectorAll('.new-photo-img')
+                let filesCount = allFiles.length
+                document.getElementById('filesCounter').innerText = filesCount + " Фото"
+            }
+            reader.readAsDataURL(file);
+            this.value = ''
+        }
+        else {
+            alert('Недопустимый тип файла. Пожалуйста, выберите файл .png или .jpg')
+        }
+    }
+})
+
+document.getElementById('go-load-photo').addEventListener('click', function () {
+    let files = document.querySelectorAll('.new-photo-img')
+
+    let files_list = []
+    let filenames_list = []
+
+    files.forEach(file => {
+        files_list.push(file.src)
+        filenames_list.push(file.getAttribute('filename'))
+    })
+    socketio.emit('newPhotos_all', {files: files_list, filenames: filenames_list})
+})
+
+document.getElementById('cancel-load-photo').addEventListener('click', cancelLoad)
+
+
+socketio.on('newPhoto_all_result', (data) => {
+    if (data.success){
+        cancelLoad()
+    }
+    else {
+        cancelLoad()
+        alert('Произошла ошибка загрузки: ' + data.error)
+    }
+})
