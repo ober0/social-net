43dcbf6ddec6cc9de81a07f32f0089a0a6adb25d
Ruslan <onishruslan@yandex.ru>
2024-08-14 22:26:54 +0300

    the title has been updated

--- static/js/chat.js
+++ static/js/chat.js
@@ -2,7 +2,27 @@ document.addEventListener('DOMContentLoaded', function (){
     let scrollbar = document.querySelector('.message-container')
     scrollbar.scrollTop = scrollbar.scrollHeight
 
+     document.addEventListener('keydown', function (event){
+        if(event.key === 'Escape'){
+             window.location.href = '/messanger'
+        }
+    })
+
     document.querySelector('.chat-back').addEventListener('click', function (){
         window.location.href = '/messanger'
+    })
+
+    let isMessageInputActive = false
+
+    document.getElementById('message-input').addEventListener('focus', function () {
+        isMessageInputActive = true
+    })
+
+    document.getElementById('message-input').addEventListener('blur', function () {
+        isMessageInputActive = false
+    })
+
+    document.addEventListener('keydown', function (event){
+        if(event.key === 'Enter'){}
     })
 })
